51134e0ddaa274854b8e2a18f7ef357685a41217
Hung Van Nguyen <nguyenvanhung7352@gmail.com>
2022-11-04 15:15:31 +0700

    Update ChildCard.js

--- src/ListChild/ChildCard.js
+++ src/ListChild/ChildCard.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const ChildCard =({imageContent,index,onPress,nameMain,description=[],style})=>{
+const ChildCard =({imageContent,styleImage,iconMainContent,iconMainColor='#000',sizeMainIcon=60,index,onPress,nameMain,description=[],styleChildCard,styleNameMain,styleDescription})=>{
     return(
         <View>
             <TouchableHighlight
                 onPress={onPress}
-                style={[styles.card,style]}
+                style={[styles.card,styleChildCard]}
                 underlayColor='default'
             >
                 <View style={{flex:1,flexDirection:'row'}}>
-                    {imageContent?
-                        <View style={{flex:2,justifyContent:'center',alignItems:'center',borderBottomLeftRadius:10,borderTopLeftRadius:10}}>
-                            <Image source={{uri:`${imageContent}`}} style={styles.image}></Image>
+                    {iconMainContent?
+                        <View style={{flex:1.5,justifyContent:'center',alignItems:'center',borderBottomLeftRadius:10,borderTopLeftRadius:10}}>
+                            <MaterialCommunityIcons
+                                name={iconMainContent}
+                                color={iconMainColor}
+                                size={sizeMainIcon}
+                            >
+                            </MaterialCommunityIcons>
+                        </View>:
+                        imageContent?
+                        <View style={{flex:1.5,justifyContent:'center',alignItems:'center',borderBottomLeftRadius:10,borderTopLeftRadius:10}}>
+                            <Image source={{uri:`${imageContent}`}} style={[styles.image,styleImage]}></Image>
                         </View>:
                         <View></View>
                     }
-                    <View style={{flex:5,flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+                    <View style={{flex:6,flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
                         
-                        <Text style={styles.ItemName}>{nameMain}</Text>
+                        <Text style={[styles.ItemName,styleNameMain]}>{nameMain}</Text>
                         {description.map((value)=>{
                             return(
-                                <Text style={styles.ItemInfo}>{value}</Text>
+                                <Text style={[styles.ItemInfo,styleDescription]}>{value}</Text>
                             );
                         })}
                     </View>
@@ -52,6 +62,11 @@ const styles = StyleSheet.create({
     },
     ItemName:{
         fontSize:16,
+        color:'#000',
+        fontWeight:'bold'
+    },
+    ItemInfo:{
+        fontSize:14,
         color:'#000',
         fontWeight:'400'
     }
